d6513091991ba35d7f1b795cc514c0f0218f7ec0
fuzious <arpitsrivastava2012@gmail.com>
2023-08-31 15:49:34 +0530

    added bundler

--- relayer/src/server.ts
+++ relayer/src/server.ts
@@ -3,10 +3,10 @@ import bodyParser from 'body-parser';
 import cron from 'node-cron';
 import cors from 'cors';
 import { Coin, IncentivizedTestent, NibiruSigningClient, newCoin, newCoins, newSignerFromMnemonic } from "@nibiruchain/nibijs"
-import { DirectSecp256k1Wallet } from '@cosmjs/proto-signing';
+import { DirectSecp256k1HdWallet } from '@cosmjs/proto-signing';
 import { SigningCosmosClient } from '@cosmjs/launchpad';
 import { SigningCosmWasmClient } from '@cosmjs/cosmwasm-stargate';
-import { Secp256k1HdWallet } from '@cosmjs/amino'
+// import { S} from '@cosmjs/amino'
 // import { Msg, TxMessage } from "@nibiruchain/nibijs/dist/msg"
 // import {  } from "@nibiruchain/nibijs"
 // import { EncodeObject } from "@cosmjs/proto-signing";
@@ -23,10 +23,10 @@ app.use(cors());
 
 // Our queue to store UserOp objects
 const userOpQueue: any[] = [];
-const entrypointContract: string = 'nibi12kc8l3gfncxgu88trwz2gy7myyc5dw3h0m794r8da5e8yd6w5avstpkt4a';
+const entrypointContract: string = 'nibi1436kxs0w2es6xlqpp9rd35e3d0cjnw4sv8j3a7483sgks29jqwgsn6ytm8';
 
-const endpoint = "";
-const mnemonic = "crash give faint speak empower crush decade suspect cage ranch fish angry alcohol ill city";
+const endpoint = "https://rpc.itn-2.nibiru.fi:443";
+const mnemonic = "";
 
 
 // POST endpoint to add a UserOp to the queue
@@ -50,39 +50,47 @@ app.listen(PORT, () => {
 // Cron job that runs every 5 seconds
 cron.schedule('*/5 * * * * *', async () => {
     console.log('Running the cron job...');
-    const signer = await Secp256k1HdWallet.fromMnemonic(mnemonic);
+    try {
+        const signer = await DirectSecp256k1HdWallet.fromMnemonic(mnemonic);
 
-    const client = await SigningCosmWasmClient.connectWithSigner(endpoint, signer);
-    const [firstAccount] = await signer.getAccounts();
+        const client = await SigningCosmWasmClient.connectWithSigner(endpoint, signer);
+        const [firstAccount] = await signer.getAccounts();
+        console.log(firstAccount.address)
+        const incrementMsgs = []; // Array to store the EncodeObject from userOpQueue
+        while (userOpQueue.length > 0) {
+            const userOp: any = userOpQueue.shift();  // Removes the first element from the array
 
-    const incrementMsgs = []; // Array to store the EncodeObject from userOpQueue
-
-    while (userOpQueue.length > 0) {
-        const userOp: any = userOpQueue.shift();  // Removes the first element from the array
-        console.log(userOp);
-
-        // Assuming userOp can be transformed into an EncodeObject, we add to incrementMsgs
-        // You might need to modify the below transformation based on your actual data structure.
-        incrementMsgs.push({
-            typeUrl: "wasm/MsgExecuteContract",
-            value: {
-                sender: firstAccount,
-                contract: entrypointContract,
-                msg: Buffer.from(JSON.stringify(userOp.msg)).toString("base64"),
-                sent_funds: [],
-            }
-        });
+            // Assuming userOp can be transformed into an EncodeObject, we add to incrementMsgs
+            // You might need to modify the below transformation based on your actual data structure.
+            incrementMsgs.push({
+                typeUrl: "/cosmwasm.wasm.v1.MsgExecuteContract",
+                value: {
+                    sender: firstAccount,
+                    contract: entrypointContract,
+                    msg: Buffer.from(JSON.stringify(userOp)).toString("base64"),
+                    sent_funds: [],
+                }
+            });
+        }
+        console.log('test')
+        // signingClient.wasmClient.sign
+        if (incrementMsgs.length > 0) {
+            console.log('bundling.....')
+            // Assuming 'signAndBroadcast' accepts multiple messages
+            // signingClient.wasmClient.signAndBroadcast((await signer.getAccounts())[0].address, incrementMsgs, "auto");
+            const fee = {
+                amount: [{ amount: '5000', denom: 'unibi' }],  // Define appropriate fees
+                gas: '200000',  // Define appropriate gas limit
+            };
+    
+            const result = await client.execute(firstAccount.address, endpoint, incrementMsgs, fee);
+            console.log(result)
+            console.log(`Sent ${incrementMsgs.length} messages to the blockchain.`);
+        } else {
+            console.log('No messages to send.');
+        }   
+    } catch(err) {
+        console.log(err)
     }
-    // signingClient.wasmClient.sign
-    if (incrementMsgs.length > 0) {
-        // Assuming 'signAndBroadcast' accepts multiple messages
-        // signingClient.wasmClient.signAndBroadcast((await signer.getAccounts())[0].address, incrementMsgs, "auto");
-        const result = await client.execute(firstAccount.address, endpoint, incrementMsgs, "auto");
-
-        console.log(`Sent ${incrementMsgs.length} messages to the blockchain.`);
-    } else {
-        console.log('No messages to send.');
-    }
-
     // const txResp = await signingClient.sendTokens(fromAddr, toAddr, tokens, "auto");
 });
