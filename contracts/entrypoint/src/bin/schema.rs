ea06955d05f8704038d88876c958557284663429
Aniket Dixit <dixitaniket199@gmail.com>
2023-08-31 13:59:12 +0530

    limit modif

--- contracts/entrypoint/src/bin/schema.rs
+++ contracts/entrypoint/src/bin/schema.rs
@@ -1,11 +1,10 @@
 use cosmwasm_schema::write_api;
 
-use wise_wallet::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use entrypoint::msg::{ExecuteMsg, InstantiateMsg};
 
 fn main() {
     write_api! {
         instantiate: InstantiateMsg,
         execute: ExecuteMsg,
-        query: QueryMsg,
     }
 }
